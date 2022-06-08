42597f6c59559f27124d33add6fc9168c4c6255d
sp00451154 <pandasibashankar@gmail.com>
2022-06-08 20:17:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-custom-pagination-mat-table-mmjw5m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-custom-pagination-mat-table-mmjw5m)
---
+++ src/app/table-basic-example.ts
@@ -0,0 +1,186 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { startWith, tap } from 'rxjs/operators';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'HP Software and Driver', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
+@Component({
+  selector: 'table-basic-example',
+  styleUrls: ['table-basic-example.css'],
+  templateUrl: 'table-basic-example.html',
+})
+export class TableBasicExample implements OnInit {
+  displayedColumns: string[] = [
+    'position',
+    'name',
+    'symbol',
+    'action',
+  ];
+  dataSource = new MatTableDataSource<any>();
+
+  isLoading = true;
+
+  pageNumber: number = 1;
+  VOForm: FormGroup;
+  isEditableNew: boolean = true;
+  constructor(private fb: FormBuilder, private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.VOForm = this._formBuilder.group({
+      VORows: this._formBuilder.array([]),
+    });
+
+    this.VOForm = this.fb.group({
+      VORows: this.fb.array(
+        ELEMENT_DATA.map((val) =>
+          this.fb.group({
+            position: new FormControl(val.position),
+            name: new FormControl(val.name),
+            weight: new FormControl(val.weight),
+            symbol: new FormControl(val.symbol),
+            action: new FormControl('existingRecord'),
+            isEditable: new FormControl(true),
+            isNewRow: new FormControl(false),
+          })
+        )
+      ), //end of fb array
+    }); // end of form group cretation
+    this.isLoading = false;
+    this.dataSource = new MatTableDataSource(
+      (this.VOForm.get('VORows') as FormArray).controls
+    );
+    this.dataSource.paginator = this.paginator;
+
+    const filterPredicate = this.dataSource.filterPredicate;
+    this.dataSource.filterPredicate = (data: AbstractControl, filter) => {
+      return filterPredicate.call(this.dataSource, data.value, filter);
+    };
+
+    //Custom filter according to name column
+    // this.dataSource.filterPredicate = (data: {name: string}, filterValue: string) =>
+    //   data.name.trim().toLowerCase().indexOf(filterValue) !== -1;
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  goToPage() {
+    this.paginator.pageIndex = this.pageNumber - 1;
+    this.paginator.page.next({
+      pageIndex: this.paginator.pageIndex,
+      pageSize: this.paginator.pageSize,
+      length: this.paginator.length,
+    });
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.paginatorList = document.getElementsByClassName(
+      'mat-paginator-range-label'
+    );
+
+    this.onPaginateChange(this.paginator, this.paginatorList);
+
+    this.paginator.page.subscribe(() => {
+      // this is page change event
+      this.onPaginateChange(this.paginator, this.paginatorList);
+    });
+  }
+
+  applyFilter(event: Event) {
+    //  debugger;
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  // @ViewChild('table') table: MatTable<PeriodicElement>;
+  AddNewRow() {
+    // this.getBasicDetails();
+    const control = this.VOForm.get('VORows') as FormArray;
+    control.insert(0, this.initiateVOForm());
+    this.dataSource = new MatTableDataSource(control.controls);
+    // control.controls.unshift(this.initiateVOForm());
+    // this.openPanel(panel);
+    // this.table.renderRows();
+    // this.dataSource.data = this.dataSource.data;
+  }
+
+  // this function will enabled the select field for editd
+  EditSVO(VOFormElement, i) {
+    // VOFormElement.get('VORows').at(i).get('name').disabled(false)
+    VOFormElement.get('VORows').at(i).get('isEditable').patchValue(false);
+    // this.isEditableNew = true;
+  }
+
+  // On click of correct button in table (after click on edit) this method will call
+  SaveVO(VOFormElement, i) {
+    // alert('SaveVO')
+    VOFormElement.get('VORows').at(i).get('isEditable').patchValue(true);
+  }
+
+  // On click of cancel button in the table (after click on edit) this method will call and reset the previous data
+  CancelSVO(VOFormElement, i) {
+    VOFormElement.get('VORows').at(i).get('isEditable').patchValue(true);
+  }
+
+  paginatorList: HTMLCollectionOf<Element>;
+  idx: number;
+  onPaginateChange(paginator: MatPaginator, list: HTMLCollectionOf<Element>) {
+    setTimeout(
+      (idx) => {
+        let from = paginator.pageSize * paginator.pageIndex + 1;
+
+        let to =
+          paginator.length < paginator.pageSize * (paginator.pageIndex + 1)
+            ? paginator.length
+            : paginator.pageSize * (paginator.pageIndex + 1);
+
+        let toFrom = paginator.length == 0 ? 0 : `${from} - ${to}`;
+        let pageNumber =
+          paginator.length == 0
+            ? `0 of 0`
+            : `${paginator.pageIndex + 1} of ${paginator.getNumberOfPages()}`;
+        let rows = `Page ${pageNumber} (${toFrom} of ${paginator.length})`;
+
+        if (list.length >= 1) list[0].innerHTML = rows;
+      },
+      0,
+      paginator.pageIndex
+    );
+  }
+
+  initiateVOForm(): FormGroup {
+    return this.fb.group({
+      position: new FormControl(234),
+      name: new FormControl(''),
+      weight: new FormControl(''),
+      symbol: new FormControl(''),
+      action: new FormControl('newRecord'),
+      isEditable: new FormControl(false),
+      isNewRow: new FormControl(true),
+    });
+  }
+}
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
